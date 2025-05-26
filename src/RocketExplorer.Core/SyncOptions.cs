c008e1b2c9987c7592faee5a139627d795a815af
Christopher Dresel <christopher.dresel@spatial-focus.net>
2025-05-26 21:24:32 +0200

    Allow BasicAuth for RPC endpoint configuration

--- src/RocketExplorer.Core/SyncOptions.cs
+++ src/RocketExplorer.Core/SyncOptions.cs
@@ -4,7 +4,11 @@ public class SyncOptions
 {
 	public required string Environment { get; init; }
 
-	public required string RPCUrl { get; init; }
-
 	public required string RocketStorageContractAddress { get; init; }
+
+	public string RpcBasicAuthPassword { get; init; } = string.Empty;
+
+	public string RpcBasicAuthUsername { get; init; } = string.Empty;
+
+	public required string RPCUrl { get; init; }
 }
